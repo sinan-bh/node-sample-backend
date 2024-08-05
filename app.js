f24c14f8b7ba9fc186d026828469799ab6e50890
sinan <sinanbh9605@gmail.com>
2024-08-05 16:23:54 +0530

    feat: updated

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 const app = express();
 
 
-app.use(cors()); 
+app.use(cors({
+    origin: 'https://node-sample-frontend.vercel.app'
+})); 
 app.use(express.json());
 app.use((req,res,next)=>{
     console.log('start');
